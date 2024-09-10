69e74399ed7dcaf2f8c00d577a16d76e3f408f53
YohanNayanajith <yohannayanajith40@gmail.com>
2024-09-11 01:40:39 +0530

    task/change response

--- services/authService.js
+++ services/authService.js
@@ -6,7 +6,8 @@ const { logger } = require('../middleware/logger');
 const register = async (userData) => {
     const hashedPassword = await hashPassword(userData.password);
     const user = await User.create({ ...userData, password: hashedPassword });
-    const { password, ...userWithoutPassword } = user;
+    const userWithoutPassword = user.get({ plain: true });
+    delete userWithoutPassword.password;
     logger.info("User registered successfully");
     return userWithoutPassword;
 };
@@ -15,7 +16,9 @@ const login = async (email, password) => {
     logger.info(`Attempting login for user with email: ${email}`);
     const user = await User.findOne({ where: { email } });
     if (user && await comparePassword(password, user.password)) {
-        const { password, ...userWithoutPassword } = user;
+        const userWithoutPassword = user.get({ plain: true });
+        delete userWithoutPassword.password;
+
         const token = generateToken(userWithoutPassword);
         logger.info("Login successful");
         return { user: userWithoutPassword, token };
